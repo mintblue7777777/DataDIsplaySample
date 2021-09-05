6374aa73ed20a15851d65cf2954631920d1eda01
panda <mintblue7777777@gmail.com>
2021-09-05 21:48:05 +0900

    プロジェクト ファイルを追加します。

---
+++ DataDIsplaySample/GenderComboBoxItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataDisplaySample {
+     public class GenderComboBoxItem {
+        public string Label { get; set; }
+        public Gender Value { get; set; }
+
+    }
+}
---
+++ DataDIsplaySample/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataDisplaySample {
+    public enum Gender {
+        None,
+        Men,
+        Women
+    }
+    class Person {
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public bool AuthMember { get; set; }
+
+    }
+}
